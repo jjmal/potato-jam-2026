a604b9e47ec29740e44388b529cf91abe9ea2968
jjmal <jkbjmal@gmail.com>
2026-09-15 17:03:13 +0200

    fix: walls no longer possible to shoot over with a combination of not being able to shoot when too close to a wall and a ray cast extention

--- scenes/player.gd
+++ scenes/player.gd
@@ -8,7 +8,8 @@ const EPSILON = 0.01
 
 @export var shot_cooldown: float = 0.75
 var flipped: bool = false
-var can_shoot: bool = true
+var is_shot_on_cooldown: bool = false
+var is_too_close_to_wall_to_shoot: bool = false
 
 func _ready() -> void:
 	$ShootTimer.wait_time = shot_cooldown
@@ -42,8 +43,11 @@ func flip(direction):
 		scale.y = 1
 		flipped = false
 
+func can_shoot() -> bool:
+	return not is_shot_on_cooldown and not is_too_close_to_wall_to_shoot 
+
 func shoot():
-	if Input.is_action_just_pressed("shoot_forward") and can_shoot:
+	if Input.is_action_just_pressed("shoot_forward") and can_shoot():
 		var direction_state
 		if flipped:
 			direction_state = "left"
@@ -51,7 +55,7 @@ func shoot():
 			direction_state = "right"
 		needle_shot_forward.emit($Marker2D.global_position, direction_state)
 		
-		can_shoot = false
+		is_shot_on_cooldown = true
 		$ShootTimer.start()
 
 func _physics_process(delta: float) -> void:
@@ -60,4 +64,14 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_shoot_timer_timeout() -> void:
-	can_shoot = true
+	is_shot_on_cooldown = false
+
+
+func _on_prevent_shoot_next_to_wall_body_entered(body: Node2D) -> void:
+	if Utils.is_body_a_tile_set_static(body):
+		is_too_close_to_wall_to_shoot = true
+
+
+func _on_prevent_shoot_next_to_wall_body_exited(body: Node2D) -> void:
+	if Utils.is_body_a_tile_set_static(body):
+		is_too_close_to_wall_to_shoot = false
