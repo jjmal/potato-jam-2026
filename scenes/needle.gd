b6f4a11430fadc7bca0ef1700a9ea416f1baf550
jjmal <jkbjmal@gmail.com>
2026-09-15 14:50:55 +0200

    fix: Needles now point to the right directions in both movement directions.

--- scenes/needle.gd
+++ scenes/needle.gd
@@ -4,11 +4,13 @@ const SPEED = 1000
 var direction_state: String
 var direction: Vector2
 #var collision_platform_enabled: bool = false
+var flipped: bool = false
 
 func _ready() -> void:
 	direction = get_direction()
 	disable_collision_platform()
-
+	flip()
+	
 func get_direction():
 	if direction_state == "left":
 		return Vector2.LEFT
@@ -25,3 +27,12 @@ func disable_collision_platform():
 func enable_collision_platform():
 	$CollisionPlatform.disabled = false
 	
+func flip():
+	if direction_state == "right":
+		rotation = PI
+		scale.y = -1
+		flipped = true
+	elif direction_state == "left":
+		rotation = 0
+		scale.y = 1
+		flipped = false
