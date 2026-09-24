5153d59caeab889fbc67c219980e7d26449ee1ab
jjmal <jkbjmal@gmail.com>
2026-09-24 20:18:59 +0200

    More reliable distance-based turning.

--- scripts/brzydal.gd
+++ scripts/brzydal.gd
@@ -7,12 +7,11 @@ extends Enemy
 @export var aggro_speed: float
 @export var controlled_speed: float
 @export var allow_jump: bool
+@export var distance_for_turning: int
 var can_aggro: bool
 var tracked_player: Node2D
 var walk_anim_play: bool = false
-var can_turn_in_pursuit: bool = true
-var turn_delay = 0.3
-var turn_delay_cooldown = 0.0
+
 	
 func _ready() -> void:
 	direction = -1.0
@@ -43,7 +42,6 @@ func move_roam_process(delta: float):
 	
 func move_aggro_process(delta: float):
 	apply_gravity(delta)
-	update_turn_delay_cooldown(delta)
 	set_x_dir_to_player()
 	
 	flip_character()
@@ -52,26 +50,21 @@ func move_aggro_process(delta: float):
 func set_x_dir_to_player():
 	if tracked_player == null:
 		return	
-	if turn_delay_cooldown > 0:
-		return
-	if not can_turn_in_pursuit:
-		return
 	
 	var arrow = (tracked_player.global_position - global_position)
+	if arrow.length() < distance_for_turning:
+		return
+
 	
 	if arrow.x >= 0:
 		direction = 1.0
 	else:
 		direction = -1.0
-	turn_delay_cooldown = turn_delay 
-	$PursuitTurnTimer.start()
-	can_turn_in_pursuit = false
+
 
 func update_tracked_player():
 	tracked_player = $AggroModule.tracked_player
 
-func update_turn_delay_cooldown(delta):
-	turn_delay_cooldown = max(turn_delay_cooldown - delta, 0.0)
 
 func _physics_process(delta: float) -> void:
 	update_tracked_player()
@@ -89,7 +82,3 @@ func play_walk_animation():
 
 func _on_aggro_module_aggro_status(aggro_stat: bool) -> void:
 	can_aggro = aggro_stat
-
-
-func _on_timer_timeout() -> void:
-	can_turn_in_pursuit = true
