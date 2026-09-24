62b2abccd282c1f32fac5788e02aa49fa2264b9c
jjmal <jkbjmal@gmail.com>
2026-09-24 11:18:15 +0200

    Possibly functional roaming movememt for the more active enemy

--- scripts/brzydal.gd
+++ scripts/brzydal.gd
@@ -2,6 +2,8 @@ extends Enemy
 
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var fall_detection_ray = $FallDetectionRay
+@onready var wall_detection_ray = $WallDetectionRay
 @export var roam_speed: float
 @export var aggro_speed: float
 @export var controlled_speed: float
@@ -14,6 +16,23 @@ func _ready() -> void:
 func move_roam_process(delta: float):
 	apply_gravity(delta)
 	flip_character()
+	if check_if_about_to_fall() or check_if_about_to_hit_a_wall():
+		direction = - direction
+	move_forward()
+
+func check_if_about_to_fall() -> bool:
+	if not fall_detection_ray.is_colliding() and is_on_floor():
+		return true
+	return false
+	
+func check_if_about_to_hit_a_wall() -> bool:
+	if wall_detection_ray.is_colliding() and is_on_floor():
+		return true
+	return false
+
+func move_forward():
+	velocity.x = direction * roam_speed
+	move_and_slide()
 	
 #func move_aggro_process(delta: float):
 	#apply_gravity(delta)
